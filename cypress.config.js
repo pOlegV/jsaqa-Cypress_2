9f7be9932f57424b2804996016fbb5cd025e02b9
pOlegV <oleg-1@yandex.ru>
2023-08-08 10:30:29 +0300

    Add 2 zadanie commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "3ew7si",
   e2e: {
     baseUrl: 'http://qamid.tmweb.ru/client/index.php',
     setupNodeEvents(on, config) {
